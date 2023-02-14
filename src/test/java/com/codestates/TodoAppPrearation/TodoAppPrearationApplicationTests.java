edfdb3b304a29a0ede0723bd230f1b5cdc6ec456
Hoodie1368 <jhj5375@gmail.com>
2023-02-14 14:52:02 +0900

    first commit

---
+++ src/test/java/com/codestates/TodoAppPrearation/TodoAppPrearationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.TodoAppPrearation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppPrearationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
